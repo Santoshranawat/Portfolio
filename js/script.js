02f9920cd161244f67020e90a350b0816d0672be
santosh <santosh73799@gmail.com>
2023-05-20 10:52:24 +0530

    index.html

--- js/script.js
+++ js/script.js
@@ -18,3 +18,18 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+// Contact Me 
+function contactme(){
+  
+	var name_your = $('#name').val();
+	var company_your = $('#company').val();
+	var message_your = $('#message').val();
+
+	var url = "https://wa.me/+917379988894?text="
+		+ "Name: " + name_your + "%0a"
+		+ "Company: " + company_your + "%0a"
+		+ "Message: " + message_your;
+
+	window.open(url, '_blank').focus();
+
+}

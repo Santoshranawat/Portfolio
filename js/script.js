f13f942d0e74f3b12b913f2b198f05762fb6b23f
santosh <santosh73799@gmail.com>
2023-06-21 18:28:52 +0530

    add project section

--- js/script.js
+++ js/script.js
@@ -2,30 +2,30 @@
 let mybutton = document.getElementById("scrolldown");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 }
 
 // Contact Me 
-function contactme(){
-  
-	var name_your = $('#name').val();
-	var company_your = $('#company').val();
-	var message_your = $('#message').val();
+function contactme() {
 
-	var url = "https://wa.me/+917379988894?text="
+	let name_your = $('#name').val();
+	let company_your = $('#company').val();
+	let message_your = $('#message').val();
+
+	let url = "https://wa.me/+917379988894?text="
 		+ "Name: " + name_your + "%0a"
 		+ "Company: " + company_your + "%0a"
 		+ "Message: " + message_your;
